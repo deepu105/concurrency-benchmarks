245e12154abe948354f7ed0fdc6fd8f659c78621
deepu105 <d4udts@gmail.com>
2020-11-26 19:12:25 +0100

    rust tokio webserver

---
+++ rust_tokiows/src/main.rs
@@ -0,0 +1,47 @@
+extern crate env_logger;
+extern crate futures;
+extern crate tokio_proto;
+extern crate tokio_service;
+extern crate tokio_minihttp;
+
+use std::io;
+
+use futures::future;
+use tokio_minihttp::{Request, Response, Http};
+use tokio_proto::TcpServer;
+use tokio_service::Service;
+
+use std::thread;
+use std::time::Duration;
+/// `HelloWorld` is the *service* that we're going to be implementing to service
+/// the HTTP requests we receive.
+///
+/// The tokio-minihttp crate, and much of Tokio itself, are centered around the
+/// concept of a service for interoperability between crates. Our service here
+/// carries no data with it.
+///
+/// Note that a new instance of `HelloWorld` is created for each TCP connection
+/// we service, created below in the closure passed to `serve`.
+struct HelloWorld;
+
+impl Service for HelloWorld {
+    type Request = Request;
+    type Response = Response;
+    type Error = io::Error;
+    type Future = future::Ok<Response, io::Error>;
+
+    fn call(&self, _request: Request) -> Self::Future {
+        let mut resp = Response::new();
+        println!("New req");
+        // thread::sleep(Duration::from_secs(50));
+        resp.body("Hello, world!");
+        future::ok(resp)
+    }
+}
+
+fn main() {
+    drop(env_logger::init());
+    let addr = "0.0.0.0:8080".parse().unwrap();
+    TcpServer::new(Http, addr)
+        .serve(|| Ok(HelloWorld));
+}
