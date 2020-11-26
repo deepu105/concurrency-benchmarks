83c0e0991b234bbc5ed53c45bee84dbb2c578607
deepu105 <d4udts@gmail.com>
2020-11-26 19:12:13 +0100

    add rust webserver

--- rustws/src/lib.rs
+++ rustws/src/lib.rs
@@ -49,14 +49,12 @@ struct Worker {
     thread: thread::JoinHandle<()>,
 }
 
-// --snip--
-
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
             let job = receiver.lock().unwrap().recv().unwrap();
 
-            println!("Worker {} got a job; executing.", id);
+            // println!("Worker {} got a job; executing.", id);
 
             job();
         });
--- rustws/src/main.rs
+++ rustws/src/main.rs
@@ -7,34 +7,54 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(8);
+
+    let mut count = 0;
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
+        count = count + 1;
+        let count_n = Box::new(count);
+        // println!("New request {}", count_n);
         pool.execute(|| {
-            handle_connection(stream);
+            handle_connection(stream, count_n);
         });
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, count: Box<i64>) {
     let mut buffer = [0; 1024];
+    // let mut reader = BufReader::new(&stream);
+    // let mut msg = String::new();
+    // reader.read_line(&mut msg).expect("RECEIVE FAILURE!!!");
+    // println!("{}", msg);
     stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let sleep = b"GET /sleep HTTP/1.1\r\n";
+    // let get = b"GET / HTTP/1.0\r\n";
+    // let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
-    };
+    // let (status_line, filename) = if buffer.starts_with(get) {
+    if (*count % 10) == 0 {
+        println!("Adding delay. Count: {}", count);
+        thread::sleep(Duration::from_secs(2));
+    }
+    // ("HTTP/1.0 200 OK\r\n\r\n", "hello.html")
+    // } else if buffer.starts_with(sleep) {
+    //     println!("Adding delay. Count: {}", count);
+    //     thread::sleep(Duration::from_secs(10));
+    //     ("HTTP/1.1 200 OK\r\n\r\n", body)
+    // } else {
+    //     // println!("Fail. Count: {}", count);
+    //     ("HTTP/1.0 404 NOT FOUND\r\n\r\n", "404.html")
+    // };
 
+    // if (*count % 10) == 0 {
+    //     println!("Adding delay. Count: {}", count);
+    //     thread::sleep(Duration::from_secs(1));
+    // }
+
+    let (status_line, filename) = ("HTTP/1.1 200 OK\r\n\r\n", "hello.html");
     let contents = fs::read_to_string(filename).unwrap();
 
     let response = format!("{}{}", status_line, contents);
