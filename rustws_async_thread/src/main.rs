291eb149ecb6c99307a1b95a8a9bf3bdd09d43ed
deepu105 <d4udts@gmail.com>
2020-11-26 19:12:59 +0100

    rust async threadpool webserver

---
+++ rustws_async_thread/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+extern crate futures;
+use futures::executor::ThreadPoolBuilder;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    let mut pool_config = ThreadPoolBuilder::new();
+    pool_config.pool_size(8);
+    let pool = pool_config.create().expect("couldn't create threadpool");
+
+    let mut count = 0;
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        count = count + 1;
+        let count_n = Box::new(count);
+
+        pool.spawn_ok(async {
+            handle_connection(stream, count_n).await;
+        });
+    }
+}
+
+async fn handle_connection(mut stream: TcpStream, count: Box<i64>) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    if (*count % 10) == 0 {
+        println!("Adding delay. Count: {}", count);
+        thread::sleep(Duration::from_secs(2));
+    }
+
+    let (status_line, filename) = ("HTTP/1.1 200 OK\r\n\r\n", "hello.html");
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
