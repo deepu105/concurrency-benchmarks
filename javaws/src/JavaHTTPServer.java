920957e2fed240e5ef644e925eea7c40bbad7679
deepu105 <d4udts@gmail.com>
2020-11-29 18:19:25 +0100

    update java sample

--- javaws/src/JavaHTTPServer.java
+++ javaws/src/JavaHTTPServer.java
@@ -2,48 +2,44 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class JavaHTTPServer implements Runnable {
-
-    private final Socket connect;
-    private final int count;
-
-    private JavaHTTPServer(Socket c, int cnt) {
-        count = cnt;
-        connect = c;
-    }
-
+public class JavaHTTPServer {
     public static void main(String[] args) {
         var count = 0;
-        var PORT = 8080;
-        try {
-            var serverConnect = new ServerSocket(PORT);
-            System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
-            // we listen until user halts server execution
+        int port = 8080;
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is listening on port " + port);
             while (true) {
                 count++;
-                var myServer = new JavaHTTPServer(serverConnect.accept(), count);
-                // create dedicated thread to manage the client connection
-                new Thread(myServer).start();
+                new ServerThread(serverSocket.accept(), count).start();
             }
-        } catch (IOException e) {
-            System.err.println("Server Connection error : " + e.getMessage());
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
         }
     }
+}
 
-    @Override
+class ServerThread extends Thread {
+
+    private final Socket socket;
+    private final int count;
+
+    public ServerThread(Socket socket, int count) {
+        this.socket = socket;
+        this.count = count;
+    }
+
     public void run() {
         var file = new File("hello.html");
         try (
                 // we get character output stream to client (for headers)
-                var out = new PrintWriter(connect.getOutputStream());
+                var out = new PrintWriter(socket.getOutputStream());
                 // get binary output stream to client (for requested data)
-                var dataOut = new BufferedOutputStream(connect.getOutputStream());
-                var fileIn = new FileInputStream(file)
-        ) {
+                var dataOut = new BufferedOutputStream(socket.getOutputStream());
+                var fileIn = new FileInputStream(file)) {
             // add 2 second delay to every 10th request
             if (count % 10 == 0) {
                 System.out.println("Adding delay. Count: " + count);
-                Thread.sleep(2000);
+                // Thread.sleep(2000);
             }
             var fileLength = (int) file.length();
             var fileData = new byte[fileLength];
@@ -61,11 +57,10 @@ public class JavaHTTPServer implements Runnable {
             dataOut.write(fileData, 0, fileLength);
             dataOut.flush();
         } catch (Exception ex) {
-            ex.printStackTrace();
             System.err.println("Error with exception : " + ex);
         } finally {
             try {
-                connect.close(); // we close socket connection
+                socket.close(); // we close socket connection
             } catch (Exception e) {
                 System.err.println("Error closing stream : " + e.getMessage());
             }
